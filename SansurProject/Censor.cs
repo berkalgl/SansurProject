29c1cc05477ca2ee93ca91d3fd1e1a5072e8e1e8
berkalgl <berkalgull@gmail.com>
2017-11-11 16:58:31 +0300

    Fix

--- SansurProject/Censor.cs
+++ SansurProject/Censor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace SansurProject
@@ -7,17 +8,41 @@ namespace SansurProject
     {
         public static string CensorTheText(string _text, string[] listOfBannedWords)
         {
+            
             foreach (var word in listOfBannedWords)
             {
-                if (_text.IndexOf(word, StringComparison.OrdinalIgnoreCase) != -1)
-                {
-                    string asteriks = new string('*', word.Length);
 
-                    _text = Regex.Replace(_text, word, asteriks, RegexOptions.IgnoreCase);
+                int wordStartAt = _text.IndexOf(word, StringComparison.OrdinalIgnoreCase);
+
+                if (wordStartAt != -1)
+                {
+                    
+                    int priorIndex = 0;
+                    int lentghOfWord = word.Length;
+                    string asteriks = new string('*', lentghOfWord);
+
+                    StringBuilder result = new StringBuilder();
+
+
+                    while(wordStartAt != -1)
+                    {
+                        result.Append(_text.Substring(priorIndex, wordStartAt - priorIndex));
+                        result.Append(asteriks);
+
+                        wordStartAt += lentghOfWord;
+                        priorIndex = wordStartAt;
+
+                        wordStartAt = _text.IndexOf(word, wordStartAt, StringComparison.OrdinalIgnoreCase);
+                    }
+
+                    result.Append(_text.Substring(priorIndex));
+
+                    _text = result.ToString();
+
                 }
             }
 
-            return _text;
+            return _text.ToString();
         }
     }
 }
--- SansurProject/Program.cs
+++ SansurProject/Program.cs
@@ -7,12 +7,12 @@ namespace SansurProject
         public static void Main(string[] args)
         {
             
-            string text = "Armut1, armut, elmalar, narlar, nar olmak uzere";
-            string[] bannedWords = { "armut", "nar", "berk" };
+            string text = "armut, Armutlar1, elmalar, narlar, nar olmak uzere";
+            string[] bannedWords = { "Armut", "nar", "berk" };
 
             Console.WriteLine("Sansurlenecek Yazi: " + text.ToString());
 
-            string censoredText = Censor.CensorTheText(text, bannedWords).ToString();
+            string censoredText = Censor.CensorTheText(text, bannedWords);
 
             Console.WriteLine(censoredText);
             Console.ReadLine(); 
