021f423f2a8b18ed4aec9e37a788c7b470eec356
berkalgl <berkalgull@gmail.com>
2017-11-11 13:29:41 +0300

    Project Complete

---
+++ SansurProject/Censor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SansurProject
+{
+    public class Censor
+    {
+        public static string CensorTheText(string _text, string[] listOfBannedWords)
+        {
+            foreach (var word in listOfBannedWords)
+            {
+                if (_text.IndexOf(word, StringComparison.OrdinalIgnoreCase) != -1)
+                {
+                    string asteriks = new string('*', word.Length);
+
+                    _text = Regex.Replace(_text, word, asteriks, RegexOptions.IgnoreCase);
+                }
+            }
+
+            return _text;
+        }
+    }
+}
---
+++ SansurProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SansurProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            string text = "Armut1, armut, elmalar, narlar, nar olmak uzere";
+            string[] bannedWords = { "armut", "nar", "berk" };
+
+            Console.WriteLine("Sansurlenecek Yazi: " + text.ToString());
+
+            string censoredText = Censor.CensorTheText(text, bannedWords).ToString();
+
+            Console.WriteLine(censoredText);
+            Console.ReadLine(); 
+        }
+    }
+}
